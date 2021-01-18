2a7a05ed2ec780d87ad85efdcef984db3d630af7
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-18 18:38:37 +0200

    Collections.sort()

---
+++ src/main/java/CollectionMethods.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionMethods {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("красный", "синий", "зеленый");
+        System.out.println("Перед сортировкой: " + list);
+        Collections.sort(list);
+        System.out.println("После сортировки: " + list);
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println("После обратной сортировки: " + list);
+    }
+}
